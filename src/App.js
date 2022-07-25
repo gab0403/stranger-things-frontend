6f13fef6705886fb7d8c4105c869b7e911739f36
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-07-25 18:29:40 -0300

    feat: Requisito 6 - cria as variáveis de ambiente

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+const API_ENDPOINT = 'https://backend-supabase.herokuapp.com/users';
+
 function App() {
   return (
     <div className="App">
