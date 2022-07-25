0b69b9628d3114650beb38dafc22b174ca92b828
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-07-25 18:51:26 -0300

    fix: arruma erro de lint

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
-const API_ENDPOINT = 'https://backend-supabase.herokuapp.com/users';
-
 function App() {
   return (
     <div className="App">
